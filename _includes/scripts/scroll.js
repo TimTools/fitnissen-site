b976db53df745c6b1638772cc2b0c861a55f6c56
Tim Nissen <tim.nissen@rawworks.nl>
2025-07-09 17:06:01 +0200

    mod

---
+++ _includes/scripts/scroll.js
@@ -0,0 +1,21 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener("click", function (e) {
+        const targetID = this.getAttribute("href");
+
+        // Stop standaard gedrag
+        e.preventDefault();
+
+        const targetElement = document.querySelector(targetID);
+        if (targetElement) {
+          // Soepel scrollen
+          targetElement.scrollIntoView({
+            behavior: "smooth"
+          });
+
+          // Optioneel: Verberg de hash in de URL
+          history.replaceState(null, null, ' ');
+        }
+      });
+    });
+  });
